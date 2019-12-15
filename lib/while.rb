66f4d1095bbb2295f3b1cf80100f50b2c38e4b77
Leon Jamison <leonjamison@icloud.com>
2019-12-15 04:02:38 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,8 @@
 def using_while
-  10.times do |i|
-    if i -= 10
-  puts "Wingardium Leviosa"
-    end
+  levitation_force = 6
+
+  while levitation_force < 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end
 end
